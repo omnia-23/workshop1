1a939f6e458b4fa3fb7ac2d3c708206e69190a5f
omnia <omniakhaled239@gmail.com>
2026-04-14 20:59:25 +0200

    fix

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL);
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_URL);
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// };
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./DB/connection.js";
+// import { connectDB } from "./DB/connection.js";
 // import authRouter from "./modules/auth/auth.controller.js";
-// import userRouter from "./modules/user/user.controller.js";
+import userRouter from "./modules/user/user.controller.js";
 
 const bootstrap = async () => {
   const app = express();
